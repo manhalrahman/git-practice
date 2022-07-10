9ac0eba6c7a8e7590b3b4c4934d6bc85897855a8
Manhal <porteradmin@Porters-MacBook-Pro.local>
2022-07-10 19:35:27 +0530

    made some changes in Kotlin Practice

--- Practice/KotlinPractice/src/main/kotlin/hello.kt
+++ Practice/KotlinPractice/src/main/kotlin/hello.kt
@@ -1,33 +1,5 @@
 fun main() {
-    val division = try {
-        divide(5.0, 0.0)
-    }
-    catch(e : DivideByZeroException) {
-        0.0
-    }
-
-    println(division)
-
-    val input = try {
-        readLine()?.toInt()
-    } catch(e: NumberFormatException) {
-        0
-    } finally {
-        println("this is the finally block")
-    }
-    println("You entered $input")
 
 }
-
-class DivideByZeroException : Exception("Can't divide by zero")
-
-fun divide (a: Double, b : Double) : Double{
-    if(b == 0.0) {
-        throw DivideByZeroException()
-    }
-    return a/b
-}
-
-
 
 
